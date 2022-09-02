71be312e1f378de00984aca63855942159f3d5bc
Paulo Neto <paulodsneto@outlook.com>
2022-09-02 17:09:50 -0300

    Adicionando funcao de saque

--- main.go
+++ main.go
@@ -10,14 +10,35 @@ type ContaCorrente struct {
 }
 
 func main() {
-	contaDoPaulo := ContaCorrente{
-		titular:    "Paulo",
-		numAgencia: 333,
-		numConta:   12345,
-		saldo:      100.5,
+	// contaDoPaulo := ContaCorrente{
+	// 	titular:    "Paulo",
+	// 	numAgencia: 333,
+	// 	numConta:   12345,
+	// 	saldo:      100.5,
+	// }
+
+	// contaDaAlessandra := ContaCorrente{"Alessandra", 222, 321654, 200}
+	// fmt.Println(contaDoPaulo, contaDaAlessandra)
+
+	// var contaDoLula *ContaCorrente
+	// contaDoLula = new(ContaCorrente)
+	// contaDoLula.titular = "Lula"
+
+	// fmt.Println(*contaDoLula)
+	contaDoPaulo := ContaCorrente{}
+	contaDoPaulo.titular = "Paulo"
+	contaDoPaulo.saldo = 500
+	fmt.Println(contaDoPaulo.saldo)
+
+	fmt.Println(contaDoPaulo.Sacar(300), "\nSaldo atualizado:", contaDoPaulo.saldo)
+}
+
+func (c *ContaCorrente) Sacar(valordoSaque float64) string {
+	podeSacar := valordoSaque > 0 && valordoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valordoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
 	}
-
-	contaDaAlessandra := ContaCorrente{"Alessandra", 222, 321654, 200}
-	fmt.Println(contaDoPaulo, contaDaAlessandra)
-
 }
